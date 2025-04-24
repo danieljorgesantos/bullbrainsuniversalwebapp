c245fcad27045061c4d2e784838f09a024726a40
dani <daniel.jorge.telo.filipesantoss@gmail.com>
2025-04-24 07:39:17 +0100

    bb

--- src/app/_shared/components/footer/footer.component.ts
+++ src/app/_shared/components/footer/footer.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { footerTranslations } from './translations';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +8,19 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './footer.component.html'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
 
+  currentLanguage: string = 'pt-PT';
+  private route = inject(ActivatedRoute);
+
+  ngOnInit() {
+    const langParam = this.route.snapshot.paramMap.get('lang');
+    if (langParam) {
+      this.currentLanguage = langParam;
+    }
+  }
+
+  getTranslation(key: string): string {
+    return footerTranslations[this.currentLanguage]?.[key] || footerTranslations['en']?.[key] || '';
+  }
 }
